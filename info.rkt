54d7c4f25eba094918d2640a5d5cd4c5e691629c
Dominik Pantůček <dominik.pantucek@trustica.cz>
2019-10-08 08:57:38 +0200

    Import correct info.rkt with scribble-math build dependency - reported by Alex Harsanyi.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "futures-sort")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "scribble-math"))
 (define scribblings '(("scribblings/futures-sort.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
