ebc42259b3c1fe2f36c28c5769ddef808bf89504
Dominik Pantůček <dominik.pantucek@trustica.cz>
2019-10-09 00:37:30 +0200

    Fix deps list to be a two-element list rather than a list with just single string containing two package names.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "futures-sort")
-(define deps '("base scribble-lib"))
+(define deps '("base" "scribble-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "scribble-math" "at-exp-lib"))
 (define scribblings '(("scribblings/futures-sort.scrbl" ())))
 (define pkg-desc "Description Here")
