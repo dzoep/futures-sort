d7fdf5df1f22748a1499dd6f4145b9be26e8b617
Dominik Pantůček <dominik.pantucek@trustica.cz>
2019-10-08 22:19:43 +0200

    Fix build and runtime dependency problems reported by pkg-build.racket-lang.org.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "futures-sort")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "scribble-math"))
+(define deps '("base scribble-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "scribble-math" "at-exp-lib"))
 (define scribblings '(("scribblings/futures-sort.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
